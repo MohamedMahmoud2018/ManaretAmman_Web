41f1ef38c8a00df77db6c9cad37830f2c87f6d87
Hanaa Mohammed Gad <hanaamohammedgad396@gmail.com>
2023-07-23 15:46:27 +0300

    fix

--- DataAccessLayer/DTO/EmployeeLoans/EmployeeLoansInput.cs
+++ DataAccessLayer/DTO/EmployeeLoans/EmployeeLoansInput.cs
@@ -9,17 +9,15 @@ namespace DataAccessLayer.DTO
     {
         public int ID { get; set; }
         public int EmployeeID { get; set; }
-        //public int? CreatedBy { get; set; }
-        //public DateTime? CreationDate { get; set; }
-        //public int? ModifiedBy { get; set; }
-        //public DateTime? ModificationDate { get; set; }
-        public int ProjectID { get; set; }
         public DateTime? LoanDate { get; set; }
+
         [Column(TypeName = "decimal(18, 5)")]
         public decimal? LoanAmount { get; set; }
+
         [StringLength(200)]
         public string Notes { get; set; }
-        public int? loantypeid { get; set; }
+        public int? LoantypeId { get; set; }
+
         //public int? StatusID { get; set; }
         //public int? ApprovalStatusID { get; set; }
         public int? LoanSerial { get; set; }
