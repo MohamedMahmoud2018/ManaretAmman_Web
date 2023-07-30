3a38246954f6c0e565b41a7aa1b638494d8f8538
Mohamed Mahmoud Kamel <mohamedfci2018@gmail.com>
2023-07-30 22:33:57 +0300

    Notification to balance

---
+++ BLL/Services/Notification/INotificationsService.cs
@@ -0,0 +1,16 @@
+﻿using DataAccessLayer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogicLayer.Services.Notification
+{
+    public interface INotificationsService
+    {
+        public Task<List<GetRemindersResult>> GetRemindersAsync(int projectId,int userId);
+
+
+    }
+}
