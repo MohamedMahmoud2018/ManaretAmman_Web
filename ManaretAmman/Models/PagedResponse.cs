5a9f21ba69dbcfff12d696ec2b709d4345af2d25
Hanaa Mohammed Gad <hanaamohammedgad396@gmail.com>
2023-08-07 00:15:10 +0300

    update

---
+++ ManaretAmman/Models/PagedResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace ManaretAmman.Models;
+
+public class PagedResponse<T> : ApiResponse<T>
+{
+    public int PageIndex { get; set; }
+    public int PageSize { get; set; }
+    public int TotalPages { get; set; }
+    public int TotalRecords { get; set; }
+
+    public PagedResponse(T data, int pageNumber, int pageSize) : base(true, null, null)
+    {
+        this.PageIndex = pageNumber;
+        this.PageSize   = pageSize;
+        this.Data       = data;
+    }
+}
